5184fa45386e908d6254009e83b7e4112e9d2a56
Lasse <206079734+Lasse1926@users.noreply.github.com>
2025-09-21 20:14:18 +0200

    switched from reuire lsp to vim.lsp

--- lua/configs/lspconfig.lua
+++ lua/configs/lspconfig.lua
@@ -1,25 +1,24 @@
 require("nvchad.configs.lspconfig").defaults()
 local base = require("nvchad.configs.lspconfig")
 
+
 local on_attach = base.on_attach
 local capabilities = base.capabilities
 
-local lspconfig = require("lspconfig")
 
-lspconfig.clangd.setup{
+vim.lsp.config("clangd",{
   on_attach = function(client,bufnr)
     client.server_capabilities.signatureHelpProvider = false
     on_attach(client,bufnr)
   end,
   capabilities = capabilities,
-}
-
-lspconfig.ts_ls.setup({
+})
+vim.lsp.config("ts_ls",{
   on_attach = on_attach,
   capabilities = capabilities,
 })
 
-lspconfig.eslint.setup({
+vim.lsp.config("eslint",{
   on_attach = on_attach,
   capabilities = capabilities,
 })
