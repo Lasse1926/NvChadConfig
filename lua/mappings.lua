e96604ce3b5b5fd5eadfd3967db5f5ee1a1f414b
Lasse <206079734+Lasse1926@users.noreply.github.com>
2025-10-30 13:46:59 +0100

    added vimlsp keymaps

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -6,5 +6,16 @@ local map = vim.keymap.set
 map("n", ";", ":", { desc = "CMD enter command mode" })
 map("i", "jk", "<ESC>")
 
+local opts = { noremap = true, silent = true }
+-- Show hover
+map("n", "K", vim.lsp.buf.hover, opts)
 
+-- Jump to definition
+map("n", "gd", vim.lsp.buf.definition, opts)
+
+-- Open code actions using the default LSP UI
+map("n", "<leader>ca", vim.lsp.buf.code_action, opts)
+
+-- Open code actions for the selected visual range
+map("x", "<leader>ca", vim.lsp.buf.range_code_action, opts)
 -- map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
